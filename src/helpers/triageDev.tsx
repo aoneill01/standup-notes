82062af03ad18aefeaaf289320e533d29418cba9
Andy O'Neill <aoneill01@gmail.com>
2021-06-10 14:55:35 -0400

    Updating triage dev rotation

--- src/helpers/triageDev.tsx
+++ src/helpers/triageDev.tsx
@@ -6,7 +6,7 @@ export type TriageDevs = {
 };
 
 export function getTriageDevs(date: Date): TriageDevs {
-  const developerOrder = ["AO", "RG", "KH", "DJ", "SK"];
+  const developerOrder = ["AO", "RG", "KH", "DJ", "SK", "AK"];
   const getDeveloperForIndex = (index: number) =>
     developerOrder[index % developerOrder.length];
   const week = differenceInCalendarWeeks(date, new Date(2021, 4, 17), {
